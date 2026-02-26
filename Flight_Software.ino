068e1cc9339dc3dd27a1ad34b7b8863db1f427dc
Erick Padilla <erickpadilla1945@gmail.com>
2026-02-25 17:30:24 -0700

    Update Flight_Software.ino

--- Flight_Software.ino
+++ Flight_Software.ino
@@ -14,25 +14,25 @@ MS5611 BARO(0x77);
 String dataBuffer;
 
 
+SimpleKalmanFilter GYRO_FILTER(10.47, 10.47, 1);  // Filter for the roll axis
+
 
 const int chipSelect = 53;
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
 
-const int FREQUENCY = 1666;
-const long HZ = 1 / FREQUENCY;
 const int NUM_OF_SAMPLES = 1000;    // Controls how many samples will be taken while gyroscope is being calibrated
-const int CALIBRATION_TIMER = 100;  // Controls how fast the RGB LED will blink gyroscope is being calibrated
+const int CALIBRATION_TIMER = 400;  // Controls how fast the RGB LED will blink gyroscope is being calibrated
 const int IDLE_TIMER = 2000;        // Controls how fast the RGB LED will blink while rocket idles on pad
-const int LAUNCH_TIMER = 1000;      // Controls how fast data is written into dataBuffer (Higher number means writing data more slowly)
-const int APOGEE_TIMER = 2000;      // Controls how fast the RGB LED will blink while rocket has landed
+const int LAUNCH_TIMER = 50;        // Controls how fast data is written into dataBuffer
+const int LAND_TIMER = 500;         // Controls how fast the RGB LED will blink while rocket has landed
 int COUNTER;
 
 
 // Change these to set when rocket states get changed
-const long HEIGT_ABOVE_PAD = 40;      // Minimum height above pad where rocket is considered launched / IN FT
-const long HEIGT_BELOW_APOGEE = 100;  // Minimum height below apogee where rocket is considered falling / IN FT
 const long LAUNCH_ACCEL = 35;         // Minimum G force rocket experiences to be considered launched / IN m/s^2
+const long HEIGT_ABOVE_PAD = 30;      // Minimum height above pad where rocket is considered launched / IN FT
+const long HEIGT_BELOW_APOGEE = 200;  // Minimum height below apogee where rocket is considered falling / IN FT
 
 // Used for audio & visual feedback on rocket
 const int buzzer = 5;     //Pin number for buzzer
@@ -57,7 +57,7 @@ bool LED_STATE = true;      // Used to toggle LEDs
 int state = 1;
 
 // Setup IMU variables
-float GYRO_X, GYRO_Y, GYRO_Z;                // Holds gyroscope data in X, Y, Z
+float GYRO_X, GYRO_Y, GYRO_Z, FILTER_DATA;   // Holds gyroscope data in X, Y, Z
 float GYRO_AVG_X, GYRO_AVG_Y, GYRO_AVG_Z;    // Used in gyroscope calibration
 float OFFSET_X, OFFSET_Y, OFFSET_Z;          // Used in gyroscope calibration
 float ACCEL_X, ACCEL_Y, ACCEL_Z, MAG_ACCEL;  // Holds accel data in X, Y, Z and magnitude
@@ -67,11 +67,13 @@ float PRESSURE, ALTITUDE, PAD_ALT, APOGEE;   // Holds pressure, altitude, altitu
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
   Wire.begin();
   START_UP();
   IMU_SETP();
-  // SD_SETUP();
+  SD_SETUP();
   CALIBRATE_AND_OFFSET();
+
 
   // reserve 1 kB for String used as a dataBuffer
   dataBuffer.reserve(1024);
@@ -83,8 +85,7 @@ void loop() {
     case 1:
       {
         if (currentMillis - previousMillis > IDLE_TIMER) {
-          digitalWrite(blue_led, LED_STATE);
-          tone(buzzer, 1000, 25);
+          digitalWrite(LED_BUILTIN, LED_STATE);
           previousMillis = currentMillis;
           LED_STATE = !LED_STATE;
         }
@@ -93,6 +94,7 @@ void loop() {
         GYRO_X = gyro.gyro.x + OFFSET_X;
         GYRO_Y = gyro.gyro.y + OFFSET_Y;
         GYRO_Z = gyro.gyro.z + OFFSET_Z;
+        FILTER_DATA = GYRO_FILTER.updateEstimate(GYRO_Y);
         ACCEL_X = accel.acceleration.x;
         ACCEL_Y = accel.acceleration.y;
         ACCEL_Z = accel.acceleration.z;
@@ -101,35 +103,15 @@ void loop() {
         ALTITUDE = BARO.getAltitudeFeet();
 
 
-        // Serial.print("Accel: (");
-        // Serial.print(GYRO_X, 4);
-        // Serial.print(" ");
-        // Serial.print(GYRO_Y, 4);
-        // Serial.print(" ");
-        // Serial.println(GYRO_Z, 4);
-        // Serial.print(") ");
-
-        // Serial.print("Mag: ");
-        // Serial.print(MAG_ACCEL);
-        // Serial.print(", ");
-
-        // Serial.print("Pres / Alt: (");
-        // Serial.print(PRESSURE);
-        // Serial.print(", ");
-        // Serial.print(ALTITUDE - PAD_ALT);
-        // Serial.println(")");
 
         if (MAG_ACCEL > LAUNCH_ACCEL && ACCEL_LAUNCH == false) {
-          // Serial.println("MAG SET TRUE");
           ACCEL_LAUNCH = true;
         }
         if ((ALTITUDE - PAD_ALT) > HEIGT_ABOVE_PAD && ALT_LAUNCH == false) {
-          // Serial.println("ALT SET TRUE");
           ALT_LAUNCH = true;
         }
-
         if (ACCEL_LAUNCH == true && ALT_LAUNCH == true) {
-          // Serial.println("STATE SET: 2");
+          Serial.println("State set to: 2");
           state = 2;
         }
       }
@@ -143,6 +125,7 @@ void loop() {
         GYRO_X = gyro.gyro.x + OFFSET_X;
         GYRO_Y = gyro.gyro.y + OFFSET_Y;
         GYRO_Z = gyro.gyro.z + OFFSET_Z;
+        FILTER_DATA = GYRO_FILTER.updateEstimate(GYRO_Y);
         ACCEL_X = accel.acceleration.x;
         ACCEL_Y = accel.acceleration.y;
         ACCEL_Z = accel.acceleration.z;
@@ -150,28 +133,30 @@ void loop() {
         ALTITUDE = BARO.getAltitudeFeet();
         APOGEE = max(APOGEE, ALTITUDE);
 
-        // Add all sensor data to dataBuffer string so that it can be saved to micro sd
-        if (currentMillis - previousMillis > LAUNCH_TIMER) {
-          dataBuffer += String(GYRO_X, 4);  // Turn sesor data into string and keep the first 4 decimals
-          dataBuffer += "  ";               // Leave a space between each sensor data item
-          dataBuffer += String(GYRO_Y, 4);
-          dataBuffer += "  ";
-          dataBuffer += String(GYRO_Z, 4);
-          dataBuffer += "  ";
-          dataBuffer += String(ACCEL_X, 4);
-          dataBuffer += "  ";
-          dataBuffer += String(ACCEL_Y, 4);
-          dataBuffer += "  ";
-          dataBuffer += String(ACCEL_Z, 4);
-          dataBuffer += "  ";
-          dataBuffer += String(PRESSURE, 4);
-          dataBuffer += "  ";
-          dataBuffer += String(ALTITUDE, 4);
-          dataBuffer += "\r\n";  // Start a new line for each new data point
-          previousMillis = currentMillis;
-          Serial.print("Unsaved data buffer length (in bytes): ");
-          Serial.println(dataBuffer.length());
-        }
+        dataBuffer += String(GYRO_X, 8);  // Turn sesor data into string and keep the first 8 decimals
+        dataBuffer += " ";                // Leave a space between each sensor data item
+        dataBuffer += String(GYRO_Y, 8);
+        dataBuffer += " ";
+        dataBuffer += String(GYRO_Z, 8);
+        dataBuffer += " ";
+        dataBuffer += String(FILTER_DATA, 8);
+        dataBuffer += " ";
+        dataBuffer += String(ACCEL_X, 4);
+        dataBuffer += " ";
+        dataBuffer += String(ACCEL_Y, 4);
+        dataBuffer += " ";
+        dataBuffer += String(ACCEL_Z, 4);
+        dataBuffer += " ";
+        dataBuffer += String(PRESSURE, 4);
+        dataBuffer += " ";
+        dataBuffer += String(ALTITUDE, 4);
+        dataBuffer += " ";
+        dataBuffer += String(currentMillis);
+        dataBuffer += "\r\n";  // Start a new line for each new data point
+        Serial.print("Unsaved data buffer length (in bytes): ");
+        Serial.print(dataBuffer.length());
+        Serial.print(" Time: ");
+        Serial.println(currentMillis);
 
         // check if the SD card is available to write data without blocking
         // and if the dataBuffered data is enough for the full chunk size
@@ -186,17 +171,17 @@ void loop() {
           dataBuffer.remove(0, chunkSize);
         }
 
-        if (APOGEE - ALTITUDE > HEIGT_BELOW_APOGEE) {
-          // Serial.println("STATE SET: 3");
+        if ((APOGEE - ALTITUDE) > HEIGT_BELOW_APOGEE) {
+          dataFile.close();
+          Serial.println("State set to: 3");
           state = 3;
         }
         break;
       }
     case 3:
       {
-        tone(buzzer, 500, 500);
-        if (currentMillis - previousMillis > APOGEE_TIMER) {
-          digitalWrite(green_led, LED_STATE);
+        if (currentMillis - previousMillis > LAND_TIMER) {
+          digitalWrite(LED_BUILTIN, LED_STATE);
           previousMillis = currentMillis;
           LED_STATE = !LED_STATE;
         }
@@ -208,7 +193,6 @@ void loop() {
 // Calculates the gryoscope offset
 void CALIBRATE_AND_OFFSET() {
   Serial.println("Starting calibration...");
-  delay(500);
   for (COUNTER = 0; COUNTER < NUM_OF_SAMPLES;) {
     if (IMU.gyroscopeAvailable() == 1) {
       currentMillis = millis();
@@ -228,7 +212,7 @@ void CALIBRATE_AND_OFFSET() {
       GYRO_AVG_Y += GYRO_Y;
       GYRO_AVG_Z += GYRO_Z;
       if (currentMillis - previousMillis > CALIBRATION_TIMER) {
-        digitalWrite(blue_led, LED_STATE);
+        digitalWrite(LED_BUILTIN, LED_STATE);
         previousMillis = currentMillis;
         LED_STATE = !LED_STATE;
       }
@@ -243,8 +227,6 @@ void CALIBRATE_AND_OFFSET() {
 
   BARO.read();                       //  note no error checking => "optimistic".
   PAD_ALT = BARO.getAltitudeFeet();  // Get the first height as the height above the pad
-  // Serial.print("Pad Height: ");
-  // Serial.println(PAD_ALT, 4);
   GYRO_AVG_X = 0;
   GYRO_AVG_Y = 0;
   GYRO_AVG_Z = 0;
@@ -260,9 +242,11 @@ void START_UP() {
     Serial.println("2. is your wiring correct?");
     Serial.println("3. did you change the chipSelect pin to match your shield or module?");
     Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
-    digitalWrite(red_led, HIGH);
-    while (true)
-      ;
+    while (1) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+    }
   } else {
     Serial.println("SD Card Found!");
   }
@@ -271,13 +255,13 @@ void START_UP() {
   // Check to see if barometer working
   if (BARO.begin() == true) {
     Serial.print("MS5611 found: ");
-    digitalWrite(red_led, HIGH);
-
     Serial.println(BARO.getAddress());
   } else {
     Serial.println("MS5611 not found. halt.");
     while (1) {
-      delay(10);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
     }
   }
 
@@ -285,13 +269,14 @@ void START_UP() {
   // Check to see if IMU working
   if (!IMU.begin_I2C()) {
     while (1) {
-      digitalWrite(red_led, HIGH);
-      delay(10);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
     }
   } else {
     Serial.println("LSM6DSO32 Found!");
   }
-  digitalWrite(red_led, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 
@@ -309,14 +294,12 @@ void SD_SETUP() {
 
 
   while (SD.exists(fileName)) {
-    Serial.print("File '");
-    Serial.print(fileName);
-    Serial.print("' already exists. Trying next file.");
-    Serial.println();
+    digitalWrite(LED_BUILTIN, HIGH);
     testRun += 1;
     fileName = testName;
     fileName += testRun;
     fileName += fileType;
+    digitalWrite(LED_BUILTIN, LOW);
   }
 
 
@@ -325,6 +308,8 @@ void SD_SETUP() {
   Serial.print("'. Have a safe flight!");
   Serial.println();
   dataFile = SD.open(fileName, FILE_WRITE);
+  dataFile.write("Gyro X | Gyro Y | Gyro Z | Gyro Filter | Accel X | Accel Y | Accel Z | Pressure | Altitude | Time\r\n");
+  dataFile.flush();
 }
 
 
