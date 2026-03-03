8eb0dca633cd5b5d4735411c23926e387e39d2a0
Erick Padilla <erickpadilla1945@gmail.com>
2026-03-03 12:55:54 -0700

    Update Servo.ino

--- Servo.ino
+++ Servo.ino
@@ -26,3 +26,7 @@ void loop() {
     delay(25);
   }
 }
+
+float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return float((x - in_min) * ((out_max - out_min) / (in_max - in_min))) + out_min;
+}
