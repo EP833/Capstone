6ee4b7eca8ed9d54a72034017d8a3a531334ca2b
Erick Padilla <erickpadilla1945@gmail.com>
2026-03-12 22:28:08 -0700

    Update READ_ROCKSIM.ino

--- READ_ROCKSIM.ino
+++ READ_ROCKSIM.ino
@@ -1,13 +1,25 @@
 #include <SPI.h>
 #include <SD.h>
+#include <Servo.h>
 
+
+Servo myservo;  // create servo object to control a servo
 File myFile;
+
 // Will hold data value while it is a char array
 char buffer[64];
 // Will hold data value when char array is converted to float
 float temp;
+
+// TEMP VARIABLES TO DO SERVO SIM
 // Will hold current position for buffer
-int pos = 0;
+int pos = 0;  // variable to store the servo position
+
+int MIN_SIGNAL = 630;
+int MAX_SIGNAL = 750;
+
+float MIN_DEGREE = 0.2530727415;
+float MAX_DEGREE = -0.2234021443;
 
 
 // Will be used determine which data value is being recorded
@@ -30,50 +42,56 @@ int excel_pos = 0;
 
 // Define test variables
 float Pre_Time = 0, Cur_Time = 0, Cur_yAccel = 0, Cur_yVel = 0, Cur_Alt = 0, Cur_Density = 0;
-float spin_time = 0, tolerance = 0.02;
+float spin_time = 0, backup_spin_time = 0;
 bool reached_target_1 = false;
 bool reached_target_2 = false;
 
 
 // Define constants (Units)
-const float y_bar = 0.027244444444444;    // m
-const float I_Total = 0.001243311506869;  // kg*m^2
-const float A = 0.007006590000000;        // m^2
-const float C_Nalpha = 4.690221710596553;
-const float hold_time = 2;                     // How long to hold spin (s)
-const float C_d = 0.02;                        // Coeficant of damping
-const float servo_rate_ideal = 20.944;         // rad/s
-const float servo_rate_worst = 0.0174533 * 4;  // rad/s
-const float max_angle = 0.0872665;             // 5 degree in rad
-const float min_angle = -0.0872665;            // -5 degree in rad
-
+const float y_bar = 0.027244444444444;              // m
+const float I_Total = 0.001243311506869;            // kg*m^2
+const float A = 0.007006590000000;                  // m^2
+const float C_Nalpha = 4.690221710596553;           //
+const float hold_time = 2;                          // How long to hold spin (s)
+const float backup_hold_time = 4;                   // How long to hold spin (s)
+const float C_d = 0.02;                             // Coeficant of damping
+const float servo_resolution = 0.003970624048 * 2;  // rad
+int multiple = 0;                                   //
+const float max_angle = 0.0872665;                  // 5 degree in rad
+const float min_angle = -0.0872665;                 // -5 degree in rad
 
 // Define equation variables
-float q;             // 1/2 * Density * Velocity ^2
-float AoA;           // Angle of attack (rad)
-float Prev_AoA = 0;  // Angle of attack (rad)
-float Fn = 0;        // q * A * AoA * C_Nalpha
-float Moment_A = 0;  // 4 * Fn * y_bar
-float Moment_D = 0;  // C_d * Omega
-float M_Net;
-float Omega = 0;  // Moment_A / I_Total
-float Alpha_Omega = 0;
-float n = 0;  // Omega / (2*pi)
+float q;                // 1/2 * Density * Velocity ^2
+float AoA;              // Angle of attack (rad)
+float Prev_AoA = 0;     // Angle of attack (rad)
+float Fn = 0;           // q * A * AoA * C_Nalpha
+float Moment_A = 0;     // 4 * Fn * y_bar
+float Moment_D = 0;     // C_d * Omega
+float M_Net;            //
+float Prev_n = 0;       // Omega / (2*pi)
+float n = 0;            // Omega / (2*pi)
+float Omega = 0;        // Moment_A / I_Total
+float Prev_Omega = 0;   // Moment_A / I_Total
+float Alpha_Omega = 0;  //
 
 // Define control variables
-float Kp = 3e-5, Ki = 1e-3, Kd = 0;  // Proportional, Integral ,Derivative  Gains
-float setpoint = 100;                 // Goal value (rpm)
-float error;                          // Difference between setpoint and current value
-float prev_error = 0;                 // Difference between setpoint and current value
-float integral = 0;                   // The accumalitive error in integration
+float Kp = 5.9e-6, Ki = 20e-7, Kd = 0;  // Proportional, Integral ,Derivative  Gains
+//float Kp = 3e-5, Ki = 0, Kd = 0;  // Proportional, Integral ,Derivative  Gains
+float setpoint = 0;  // Goal value (rpm)
+float error;           // Difference between setpoint and current value
+float prev_error = 0;  // Difference between setpoint and current value
+float integral = 0;    // The accumalitive error in integration
 float derivative = 0;
 float dt;           // Sampling Interval
 float dAoA;         // Rate of change of the AoA
 float AoA_des = 0;  // AoA we want
 float maxStep = 0;
-float AoA_des_last = 0;
+int servo_pos;  // variable to store the servo position
 
 void setup() {
+  myservo.attach(6);  // attaches the servo on pin 9 to the servo object
+  servo_pos = (fmap(0, MIN_DEGREE, MAX_DEGREE, MIN_SIGNAL, MAX_SIGNAL));
+  myservo.writeMicroseconds(servo_pos);
   Serial.begin(9600);
   while (!Serial) {
     ;
@@ -86,7 +104,7 @@ void setup() {
   }
   Serial.println("SD INIT");
 
-  myFile = SD.open("T_DATA.txt", FILE_READ);
+  myFile = SD.open("ARC.txt", FILE_READ);
 }
 
 void loop() {
@@ -97,12 +115,11 @@ void loop() {
     // If char is neither then add it to buffer
     // Once next char is a space or /t then make buffer into a float and clear buffer
     String list = myFile.readStringUntil('\n');
-    // Serial.println(list);
 
     int StringSize = list.length();
-    for (int i = 1; i < StringSize; i++) {
+    for (int i = 0; i < StringSize; i++) {
       // Checks to see if next character is a '\t' or a ' '
-      if (isSpace(list[i]) && i != 1) {
+      if (isSpace(list[i])) {
         buffer[pos] = '\0';
         temp = atof(buffer);
         pos = 0;
@@ -114,22 +131,12 @@ void loop() {
             }
           case 1:
             {
-              Cur_yAccel = temp;
-              break;
-            }
-          case 5:
-            {
-              Cur_Density = temp;
-              break;
-            }
-          case 6:
-            {
               Cur_yVel = temp;
               break;
             }
-          case 7:
+          case 2:
             {
-              Cur_Alt = temp;
+              Cur_Density = temp;
               break;
             }
         }
@@ -141,66 +148,73 @@ void loop() {
     }
     excel_pos = 0;
 
+
     // Perform physics equations
     dt = Cur_Time - Pre_Time;
     n = RPM_Eqn(Omega);
     q = Q_Eqn(Cur_Density, Cur_yVel);
+    Moment_D = MoD_Eqn(Omega, C_d);
 
-    //      ---- CONTROL LOGIC: Reach -> Hold for 2s -> Release ----
-    if (Cur_Time >= 4 && (reached_target_1 == false || reached_target_2 == false)) {
+    //  ---- CONTROL LOGIC: Reach -> Hold for 2s -> Release ----
+    if (Cur_Time >= 3 && (reached_target_1 == false || reached_target_2 == false)) {
       // Find the error between the setpoint and the current RPM
       error = setpoint - n;
-      maxStep = dt * servo_rate_worst;
       derivative = (error - prev_error) / dt;
       integral = integral + error * dt;
-      AoA_des = Kp * error + Ki * integral + Kd * derivative;
-      // AoA = AoA + max(-maxStep, min(maxStep, AoA_des - AoA));  // KEEP
-      AoA = AoA_des;                              //
+      AoA += Kp * error + Ki * integral + Kd * derivative;
       AoA = max(min_angle, min(max_angle, AoA));  // Does not allow the AoA to be greater than 5° or less than -5°
+      servo_pos = fmap(AoA, MIN_DEGREE, MAX_DEGREE, MIN_SIGNAL, MAX_SIGNAL);
+      myservo.writeMicroseconds(servo_pos);
 
-      if ((abs(n) > (abs(setpoint) - 10)) && (abs(n) < (abs(setpoint) + 10))) {
+      if ((abs(n) > (abs(setpoint) - 2)) && (abs(n) < (abs(setpoint) + 2))) {
         spin_time += dt;
+      }
+      if ((abs(n) > (abs(setpoint) - 10)) && (abs(n) < (abs(setpoint) + 10))) {
+        backup_spin_time += dt;
       }
 
 
       // If held for set time swap to next setpoint
-      if (spin_time >= hold_time) {
+      if (spin_time >= hold_time || backup_spin_time >= backup_hold_time) {
         setpoint = -setpoint;
         // If first setpoint flag true then make the second one true
         if (reached_target_1 == true) {
           reached_target_2 = true;
+          servo_pos = fmap(0, MIN_DEGREE, MAX_DEGREE, MIN_SIGNAL, MAX_SIGNAL);
+          myservo.writeMicroseconds(servo_pos);
         }
         // Set first setpoint flag true
         reached_target_1 = true;
         // Reset spin time
+        // integral = 0;
         spin_time = 0;
+        backup_spin_time = 0;
       }
       prev_error = error;
     } else {
-      AoA = 0.00005;
+      AoA = 0.0005;
     }
 
     // Calculate physics equations
     Fn = Fn_Eqn(A, C_Nalpha, q, AoA);
     Moment_A = MoR_Eqn(Fn, y_bar);
-    Moment_D = MoD_Eqn(Omega, C_d);
     M_Net = Moment_A - Moment_D;
     Alpha_Omega = Omega_Eqn(M_Net, I_Total);
     Omega = Omega + Alpha_Omega * dt;
     Serial.print("RPM = ");
-    Serial.print(n);
+    Serial.print(n, 5);
     Serial.print(" | ");
     Serial.print("Error = ");
     Serial.print(error, 5);
-    Serial.print(" | ");
-    Serial.print("AoA = ");
-    Serial.print(AoA, 8);
     Serial.print(" | ");
     Serial.print("integral = ");
     Serial.print(integral, 8);
     Serial.print(" | ");
     Serial.print("Spin Time = ");
-    Serial.print(spin_time, 5);
+    Serial.print(spin_time, 4);
+    Serial.print(" | ");
+    Serial.print("Backup Spin Time = ");
+    Serial.print(backup_spin_time, 4);
     Serial.print(" | ");
     Serial.print("Time = ");
     Serial.print(Cur_Time, 4);
@@ -231,7 +245,7 @@ float RPM_Eqn(float w) {
 
 float MoR_Eqn(float Fn, float Y_Bar) {
   float result;
-  result = 8 * Fn * Y_Bar;
+  result = 4 * Fn * Y_Bar;
   return result;
 }
 
@@ -251,4 +265,9 @@ float Fn_Eqn(float Fin_Area, float C, float Q, float Angle_of_Attack) {
   float result;
   result = Fin_Area * C * Q * Angle_of_Attack;
   return result;
+}
+
+
+int fmap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return int((x - in_min) * ((out_max - out_min) / (in_max - in_min))) + out_min;
 }
