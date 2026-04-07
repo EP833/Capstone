5e456e1e0e8d5a569e4e4d670fe98f24f87a8aef
Erick Padilla <erickpadilla1945@gmail.com>
2026-04-06 18:46:09 -0700

    Update Servo.ino

--- Servo.ino
+++ Servo.ino
@@ -1,32 +1,74 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myServo;
 
-// twelve servo objects can be created on most boards
 
-int pos = 0;  // variable to store the servo position
+/*
+int LOW_SIGNAL = 710
+Fin 1: 79.7 degrees
+Fin 2: 79.0 degrees
+Fin 3: 80.7 degrees
+Fin 4: 80.6 degrees
+Fin Avg: 80 degrees
+*/
+
+/*
+int HIGH_SIGNAL = 630
+Fin 1: 98.6 degrees
+Fin 2: 99.1 degrees
+Fin 3: 99.2 degrees
+Fin 4: 99.9 degrees
+Fin Avg = 99.2 degrees
+*/
+
+int LOW_SIGNAL = 710;
+float LOW_SIGNAL_ANGLE = -0.1745329252;  // rad
+int HIGH_SIGNAL = 630;
+float HIGH_SIGNAL_ANGLE = 0.1605702912;  // rad
+float fin_angle;
+int pos;
+char KEEP_SIGNAL[10];
+
+
+int state = 1;
+
+
+String inputString = "";
+bool inputComplete = false;
 
 void setup() {
-  myservo.attach(6);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
+  myServo.attach(6);  // servo signal pin
+  inputString.reserve(10);
+
+  Serial.println(fmap(-0.0872664626, LOW_SIGNAL_ANGLE, HIGH_SIGNAL_ANGLE, LOW_SIGNAL, HIGH_SIGNAL));
 }
 
 void loop() {
 
-    myservo.writeMicroseconds(pos);
-
-  for (pos =600; pos <= 1200; pos += 10) {
+  if (inputComplete) {
+    pos = inputString.toInt();
+    myServo.writeMicroseconds(pos);
+    Serial.print("Servo Command: ");
     Serial.println(pos);
-    myservo.writeMicroseconds(pos);
-    delay(25);
-  }
-  for (pos = 1200; pos >= 600; pos -= 10) {
-    Serial.println(pos);
-    myservo.writeMicroseconds(pos);
-    delay(25);
+    // clear input
+    inputString = "";
+    inputComplete = false;
   }
 }
 
-float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
-  return float((x - in_min) * ((out_max - out_min) / (in_max - in_min))) + out_min;
+// This runs automatically when serial data arrives
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+
+    if (inChar == '\n') {
+      inputComplete = true;
+    }
+  }
+}
+
+int fmap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return int((x - in_min) * ((out_max - out_min) / (in_max - in_min))) + out_min;
 }
